4fd7bc6116a707fd6ab001a26170fbe55c8c9b9f
bitelon <daniel.trivicevic@gmail.com>
2026-04-01 22:14:22 +0200

    start news-feed project

---
+++ src/main/java/com/example/newsagent/NewsAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newsagent/NewsAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
